572dfa83bbf148a9f89b9b752a7f46c84ab68aa3
TatyanaVoloshko <106927709+TatyanaVoloshko@users.noreply.github.com>
2023-05-20 16:07:12 +0200

    fixed task2

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -32,7 +32,7 @@ export const App = () => {
           Task 2
         </Heading>
         <Statistics title="Main Statistics" stats={data} />
-<Statistics stats={data} />
+        <Statistics stats={data} />
 
         <Heading marginTop="50px" marginBottom="50px" textAlign="center">
           Task 3
